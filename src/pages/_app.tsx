7d99c94b1a6cf39e00a74bbff4cb11655860b5b2
hardkill551 <hardkill194@gmail.com>
2023-09-01 21:09:24 -0300

    feat/gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,30 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Analytics from '@/Components/Analytics'
+import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    function handleRouteChange(url: any) {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
     return (
     <>
       <Head>
         <title>Lokinho Skins</title>
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
