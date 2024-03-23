271deabf7b87b2c02cc5d2f6f2a0f98968f49e3c
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-03-23 20:46:04 -0300

    Alterações da página de SigUp

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,4 +1,4 @@
-import { signUpInput } from "../../utils/inputs";
+import { signUpInput, signUpInputType } from "../../utils/inputs";
 import { useRouter } from "next/router";
 import style from "./styles/SignUp.module.css";
 import { FormEvent, useContext, useEffect, useState } from "react";
@@ -11,6 +11,16 @@ import Post from "@/images/Post.png"
 import Login from "../sign-in";
 
 const Cadastro = () => {
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      // Aqui você pode lidar com o arquivo como quiser, por exemplo, enviá-lo para o servidor.
+    }
+  };
+
   return (
     <div className={style.background}>
       <div className={style.left}>
@@ -18,18 +28,25 @@ const Cadastro = () => {
         <Image src={Post} alt="background"/>
       </div>
       <div className={style.right}>
-      <form>
-        {signUpInput.map((input, index) => (
-          <div key={index}>
-            <label htmlFor={input}></label>
-            <input type={input === 'senha' ? 'password' : 'text'} id={input} name={input} placeholder={input}/>
-          </div>
-        ))}
-        <button type="submit">Enviar</button>
-      </form>
+        <form>
+          {signUpInput.map((input, index) => (
+            <div key={index}>
+              <label htmlFor={input}></label>
+              {input === "Imagem de Perfil" ? (
+                <div className={style.inputImageContainer}>
+                  <input type={signUpInputType[index]} id={input} name={input} placeholder={input} className={style.inputImage} onChange={handleFileChange} />
+                  <label htmlFor={input} className={style.chooseImageLabel}>Escolher Imagem</label>
+                  {fileName && <span className={style.fileName}>{fileName}</span>}
+                </div>
+              ) : (
+                <input type={input === 'senha' ? 'password' : 'text'} id={input} name={input} placeholder={input}/>
+              )}
+            </div>
+          ))}
+          <button type="submit">Enviar</button>
+        </form>
       </div>
     </div>
-  )
-}
-
+  );
+};
 export default Cadastro
--- src/utils/inputs.tsx
+++ src/utils/inputs.tsx
@@ -1,3 +1,3 @@
 export const loginInput = ["e-mail", "senha"]
-export const signUpInput = ["Nome", "e-mail", "senha", "Tradelink","Imagem de Perfil"]
+export const signUpInput = ["Nome", "e-mail", "senha", "Tradelink", "Imagem de Perfil"]
 export const signUpInputType = ["text", "email", "senha", "url", "file"]
