705cf824d66d393439a04379cb6909283dcdc5ca
Lucas Araujo <lucasao744@gmail.com>
2024-04-16 11:02:36 -0300

    feature/rolette

--- src/pages/roulette/index.tsx
+++ src/pages/roulette/index.tsx
@@ -1,8 +1,11 @@
-import style from "./styles/Roulette.module.css";
+import style from "./styles/roulette.module.css";
 import { FormEvent, useContext, useEffect, useState } from "react";
 const Roulette = () => {
   return (
-    <h1>Tela da releta</h1>
+
+      <h1 className={style.page}>Tela da releta</h1>
+
+    
   );
 };
 export default Roulette;
