8e802944ad2242033b3eac569e553ef9c52aa7f6
Ana Luisa <aninha.motta25@gmail.com>
2024-04-16 09:33:18 -0300

    feature/sign-in

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import style from "./styles/home.module.css";
+
+const Home = () => {
+ 
+
+  return (
+    <>
+    home
+    </>
+  );
+};
+
+export default Home;
