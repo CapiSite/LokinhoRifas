1376aab3aed85500c73927f3609da6eff2347403
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-04-23 15:32:13 -0300

    f

--- src/pages/twitch/index.tsx
+++ src/pages/twitch/index.tsx
@@ -11,26 +11,26 @@ const Twitch = () => {
 
   return (
     <div className={style.container}>
-      <Image src={Background} alt="Papel de Parede do site" className={style.wallpaper}/>
+      <Image src={Background} alt="Papel de Parede do site" className={style.wallpaper} />
       <menu className={style.navBar}>
         <div className={style.containerLogo}>
-          <Image src={logovermelho} alt="Logo do Site - LokinhoRifas" className={style.Logo}/>
+          <Image src={logovermelho} alt="Logo do Site - LokinhoRifas" className={style.Logo} />
         </div>
-          <nav>
-            <ul className={style.ItensNavBar}>
-              <li>Home</li>
-              <li>Sorteio</li>
-              <li>Live</li>
-              <li>Últimos Ganhadores</li>
-              <li>Sobre nós</li>
-            </ul>
-          </nav>
+        <nav>
+          <ul className={style.ItensNavBar}>
+            <li>Home</li>
+            <li>Sorteio</li>
+            <li>Live</li>
+            <li>Últimos Ganhadores</li>
+            <li>Sobre nós</li>
+          </ul>
+        </nav>
         <div className={style.containerBotao}>
           <button type="button" className={style.botaoEntrar}>Entrar</button>
         </div>
       </menu>
       <main className={style.conteudo}>
-        <Image src={logovermelho} alt="Logo do Site - LokinhoRifas" className={style.LogoTwitch}/>
+        <Image src={logovermelho} alt="Logo do Site - LokinhoRifas" className={style.LogoTwitch} />
         <h1 className={style.tituloLive}>RIFA IRÁ COMEÇAR AS XX:XX</h1>
         <iframe className={style.Live} width="560" height="315" src="https://www.youtube.com/embed/8MmSc9_9ULk?si=Nb6KxOKVCDSY1VuZ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
       </main>
