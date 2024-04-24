51b8b943ddd94306c7525d4a70d2b5ef4d9647a4
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-04-24 17:12:58 -0300

    feat/tela home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,20 +4,49 @@ import Image from "next/image";
 
 import Logo from '../about/images/banner_mob2.png'
 import Background from '@/images/background.png'
+import Exemplo from '@/images/lapis.png'
 
 const Home = () => {
- 
+
 
   return (
-    <>
-      <div className={style.Conteudo}>
-        <Image src={Background} alt="" className={style.Background}/>
-        <Image src={Logo} alt="" className={style.Logo}/>
-        <button className={style.botaoComprarRifa}>
-          Comprar Rifa
-        </button>
+    <div className={style.Conteudo}>
+      <Image src={Background} alt="" className={style.Background} />
+      <Image src={Logo} alt="" className={style.Logo} />
+      <button className={style.botaoComprarRifa}>
+        Comprar Rifa
+      </button>
+      <div className={style.infoRifa}>
+        <p>Próxima Rifa</p>
+        <div className={style.horaRifa}>
+          <p>0</p>
+          <p>:</p>
+          <p>08</p>
+          <p>:</p>
+          <p>50</p>
+          <p>:</p>
+          <p>23</p>
+        </div>
       </div>
-    </>  
+      <p className={style.AvisoSkins}>Últimas Skins</p>
+      <div className={style.SkinsRifa}>
+        <div>
+          <Image src={Exemplo} alt="" className={style.ImagemRifa} />
+          <Image src={Exemplo} alt="" className={style.ImagemRifa} />
+          <Image src={Exemplo} alt="" className={style.ImagemRifa} />
+        </div>
+        <div>
+          <Image src={Exemplo} alt="" className={style.ImagemRifa} />
+          <Image src={Exemplo} alt="" className={style.ImagemRifa} />
+          <Image src={Exemplo} alt="" className={style.ImagemRifa} />
+        </div>
+      </div>
+      <Image src={Background} alt="" className={style.Background} />
+      <Image src={Logo} alt="" className={style.Logo} />
+      <button className={style.botaoComprarRifa}>
+        Comprar Rifa
+      </button>
+    </div>
   );
 };
 
