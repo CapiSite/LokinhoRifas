a1dd8c3819f684d6340bc6a5c081f04ac378ce4b
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-04-24 15:33:40 -0300

    feat/tela home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,23 @@
 import { useState, useEffect } from "react";
 import style from "./styles/home.module.css";
+import Image from "next/image";
+
+import Logo from '../about/images/banner_mob2.png'
+import Background from '@/images/background.png'
 
 const Home = () => {
  
 
   return (
-    <div>
-
-    </div>
+    <>
+      <div className={style.Conteudo}>
+        <Image src={Background} alt="" className={style.Background}/>
+        <Image src={Logo} alt="" className={style.Logo}/>
+        <button className={style.botaoComprarRifa}>
+          Comprar Rifa
+        </button>
+      </div>
+    </>  
   );
 };
 
