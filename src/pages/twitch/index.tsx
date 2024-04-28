c83d12ef5e98b7405f71e7f49b854a49aaaec35e
lucas araujo <lucasao744@gmail.com>
2024-04-28 13:02:25 -0300

    Route navbar

--- src/pages/twitch/index.tsx
+++ src/pages/twitch/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import HomeIcon from '@mui/icons-material/Home';
+// import HomeIcon from '@mui/icons-material/Home';
 
 import style from "./styles/Twitch.module.css"
 import Image from "next/image";
 import Background from "@/images/background.png";
 import Post from "@/images/Post.png";
 import logovermelho from "@/images/logovermelho.png";
-
+import  { useRouter } from "next/router";
+//import { useAuth } from '';
 const Twitch = () => {
-
+  const router = useRouter();
+  //const { isLoggedIn } = useAuth();
+  
   return (
     <div className={style.container}>
       <Image src={Background} alt="Papel de Parede do site" className={style.wallpaper} />
@@ -18,14 +21,15 @@ const Twitch = () => {
         </div>
         <nav>
           <ul className={style.ItensNavBar}>
-            <li>Home</li>
-            <li>Sorteio</li>
-            <li>Live</li>
-            <li>Últimos Ganhadores</li>
-            <li>Sobre nós</li>
+            <li onClick={() => router.push("/home")}>Home</li>
+            <li onClick={() => router.push("/sorteio")}>Sorteio</li>
+            <li onClick={() => router.push("/twitch")}>Live</li>
+            <li onClick={() => router.push("/ultimosganhadore")}>Últimos Ganhadores</li>
+            <li onClick={() => router.push("/about")}>Sobre nós</li>
           </ul>
         </nav>
         <div className={style.containerBotao}>
+          {/* onClick={() => router.push(isLoggedIn ? '/perfil' : '/sign-in')} colocar no bortao abaixo vara verificar se esta logado ou n */}
           <button type="button" className={style.botaoEntrar}>Entrar</button>
         </div>
       </menu>
