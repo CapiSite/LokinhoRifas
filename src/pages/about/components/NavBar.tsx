ce2cf2fed862882fbd5c627df2cd60dbdda3f6d9
Hugo Rodrigues Costa <hugo.rc@dinfor.uniceub>
2024-04-29 09:58:17 -0300

    fix

--- src/pages/about/components/NavBar.tsx
+++ src/pages/about/components/NavBar.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useState } from "react";
 
-import Link from "next/link";
 import Image from "next/image";
 import style from '../styles/NavBar.module.css'
 import logovermelho from "@/images/logovermelho.png";
+import  { useRouter } from "next/router";
+import Link from "next/link";
 
 const NavBar = () => {
-
+  const router = useRouter();
+  //const { isLoggedIn } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -17,23 +19,24 @@ const NavBar = () => {
         <div className={style.Container}>
           <Image src={logovermelho} alt="Logo do site" className={style.Logo} />
           <div className={`${isOpen ? 'style.ContainerRoutes' : style.meuComponenteFlex}`}>
-            <Link href="/home" className={style.Routes}>
+            <Link onClick={() => router.push("/home")} className={style.Routes} href={""}>
               Home
             </Link>
-            <Link href="/" className={style.Routes}>
+            <Link onClick={() => router.push("/roulette")} className={style.Routes} href={""}>
               Sorteio
             </Link>
-            <Link href="/twitch" className={style.Routes}>
+            <Link  onClick={() => router.push("/twitch")} className={style.Routes} href={""}>
               Live
             </Link>
-            <Link href="/" className={style.Routes}>
+            <Link onClick={() => router.push("/winners")} className={style.Routes}href={""}>
               Últimos Ganhadores
             </Link>
-            <Link href="/" className={style.Routes}>
+            <Link onClick={() => router.push("/about")} className={style.Routes}href={""}>
               Sobre nós
             </Link>
           </div>
           <button type="button">
+             {/* onClick={() => router.push(isLoggedIn ? '/perfil' : '/sign-in')} colocar no bortao abaixo vara verificar se esta logado ou n */}
             <Link href="/sign-up" className={style.BotaoEntrar}>
               Entrar
             </Link>
