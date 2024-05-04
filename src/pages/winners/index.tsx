03960d4d711e81365a2808ac17996cf6b9d3d5dd
HardKILL <hardkill194@gmail.com>
2024-05-04 08:47:36 -0300

    start/pageWinners

--- src/pages/winners/index.tsx
+++ src/pages/winners/index.tsx
@@ -1,35 +1,27 @@
 import style from "./styles/winners.module.css";
 import Image from "next/image";
-import Background from '@/images/background.png'
+import Background from "@/images/background.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Winner = () => {
   const [winners, setWinners] = useState<any>([]);
-    useEffect(()=>{
-    axios.post(process.env.NEXT_PUBLIC_REACT_NEXT_APP + "/users/winners", {}, {
-  }).then((res: any) => {
-      setWinners(res.data)
-  }).catch((err: any) => {
-      
-  })
-  },[])
-  // const array = [
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "asfsfsafsadf", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "asfsafsdsfd" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" },
-  //   { name: "hugo", skin: "dragonLore", photo: "hugo", email: "luizdacuzinho", tradeUrl: "teste" }
-  // ]
+  const [page, setPage] = useState(2);
+
+  useEffect(() => {
+    axios
+      .get(process.env.NEXT_PUBLIC_REACT_NEXT_APP + "/users/winners", {
+        params: { page: page },
+      })
+      .then((res: any) => {
+        setWinners(res.data);
+        console.log(res.data);
+      })
+      .catch((err: any) => {
+        console.log(err.response.data);
+      });
+  }, []);
+
   return (
     <div className={style.Conteudo}>
       <Image src={Background} alt="" className={style.Background} />
@@ -39,9 +31,10 @@ const Winner = () => {
         <p>Usuário</p>
         <p>Skin</p>
         <p>Prêmio</p>
-      </div>  
-      {winners.map((o:any, index:any) => {
-        const estiloIndice = index % 2 === 0 ? style.IndiceOdd : style.IndiceEven;
+      </div>
+      {winners.map((o: any, index: any) => {
+        const estiloIndice =
+          index % 2 === 0 ? style.IndiceOdd : style.IndiceEven;
         return (
           <div key={index} className={style.Ganhadores}>
             <p className={estiloIndice}>{index}</p>
@@ -51,7 +44,6 @@ const Winner = () => {
           </div>
         );
       })}
-
     </div>
   );
 };
