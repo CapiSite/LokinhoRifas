754c3e090128fb81e346dd7722c58a243a9157a2
lucas araujo <lucasao744@gmail.com>
2024-05-04 10:09:14 -0300

    steps ganhadores

--- src/pages/winners/index.tsx
+++ src/pages/winners/index.tsx
@@ -5,22 +5,29 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Winner = () => {
-  const [winners, setWinners] = useState<any>([]);
-  const [page, setPage] = useState(2);
+  const [winners, setWinners] = useState([]);
+  const [page, setPage] = useState(1); 
 
   useEffect(() => {
     axios
       .get(process.env.NEXT_PUBLIC_REACT_NEXT_APP + "/users/winners", {
         params: { page: page },
       })
-      .then((res: any) => {
+      .then(res => {
         setWinners(res.data);
-        console.log(res.data);
       })
-      .catch((err: any) => {
-        console.log(err.response.data);
+      .catch(err => {
+        console.error(err.response ? err.response.data : 'Erro ao buscar dados');
       });
-  }, []);
+  }, [page]); 
+
+  const handlePreviousClick = () => {
+    setPage(prev => Math.max(prev - 1, 1)); // Evita ir para página 0
+  };
+
+  const handleNextClick = () => {
+    setPage(prev => prev + 1);
+  };
 
   return (
     <div className={style.Conteudo}>
@@ -32,19 +39,20 @@ const Winner = () => {
         <p>Skin</p>
         <p>Prêmio</p>
       </div>
-      {winners.map((o: any, index: any) => {
-        const estiloIndice =
-          index % 2 === 0 ? style.IndiceOdd : style.IndiceEven;
-        return (
-          <div key={index} className={style.Ganhadores}>
-            <p className={estiloIndice}>{index}</p>
-            <p>{o.name}</p>
+      {winners.map((o: any, index: any) => (
+        <div key={index} className={style.Ganhadores}>
+          <p className={index % 2 === 0 ? style.IndiceOdd : style.IndiceEven}>{index + 1}</p>
+          <p>{o.name}</p>
             <p>{o.skin}</p>
             <p>{o.tradeUrl}</p>
-          </div>
-        );
-      })}
+        </div>
+      ))}
+      <div className={style.pagination}>
+        <button onClick={handlePreviousClick} className={style.next}>Anterior</button>
+        <button onClick={handleNextClick} className={style.next}>Próximo</button>
+      </div>
     </div>
   );
 };
+
 export default Winner;
