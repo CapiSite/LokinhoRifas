9efd6a5a673374fb378de9c349af1e848e85e6b5
Hugo Rodrigues Costa <hugo.rc@dinfor.uniceub>
2024-09-20 10:42:41 -0300

    Loop tela admin

--- back-app/src/middlewares/authentication-admin-middleware.ts
+++ back-app/src/middlewares/authentication-admin-middleware.ts
@@ -6,6 +6,7 @@ import { AuthenticatedRequest } from './authentication-middleware';
 
 export async function authenticateAdmin(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const userId = req.userId;
+
   if (!userId) {
     return res.status(httpStatus.UNAUTHORIZED).send({
       error: "Acesso negado. Não foi possível verificar o usuário."
@@ -26,7 +27,6 @@ export async function authenticateAdmin(req: AuthenticatedRequest, res: Response
 
     return next();
   } catch (err) {
-    console.log("Erro interno do servidor ao verificar privilégios administrativos.")
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
       error: "Erro interno do servidor ao verificar privilégios administrativos."
     });
