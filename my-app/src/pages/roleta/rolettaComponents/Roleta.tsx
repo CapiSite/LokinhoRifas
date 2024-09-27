9849af323f4f97941f75fa7750fda845c9c06a9d
Leandro Ferreira <leandro.fcode@gmail.com>
2024-09-27 19:37:38 -0300

    fix roulette with 100 or more users not spinning

--- my-app/src/pages/roleta/rolettaComponents/Roleta.tsx
+++ my-app/src/pages/roleta/rolettaComponents/Roleta.tsx
@@ -67,12 +67,12 @@ const Hero = () => {
         }
 
         <div className={style.ButtonGroup}>
-          <button disabled={!isButtonActive || !winnerIsCorrected || !winnerProperties.distanceFromCenter || participants.length === 0 || rewards.length === 0} onClick={() => manageMockWinner()} >Giro Teste</button>
+          <button disabled={!isButtonActive || !winnerIsCorrected || (!winnerProperties.distanceFromCenter && participants.length < 100) || participants.length === 0 || rewards.length === 0} onClick={() => manageMockWinner()} >Giro Teste</button>
           {availableRaffles.length > 0 && 
-          <select disabled={!isButtonActive} name='raffleSelector' className={cn(style.raffleSelector, style.mobile, (windowParams.width < 550 || participants.length > 100) && style.Visible)} onChange={(e) => selectRaffle(Number(e.target.value))}>
+          <select disabled={!isButtonActive} name='raffleSelector' className={cn(style.raffleSelector, style.mobile, (windowParams.width < 550 || participants.length >= 100) ? style.Visible : '')} onChange={(e) => selectRaffle(Number(e.target.value))}>
             {availableRaffles.map((raffle) => <option key={raffle.id} value={raffle.id}>{raffle.name}</option>)}
           </select>}
-          <button disabled={!isButtonActive || !winnerIsCorrected || !winnerProperties.distanceFromCenter || !userInfo.isAdmin || rewards.length === 0 || participants.length === 0} onClick={() => manageWinner()} >Girar Roleta</button>
+          <button disabled={!isButtonActive || !winnerIsCorrected || (!winnerProperties.distanceFromCenter && participants.length < 100) || !userInfo.isAdmin || rewards.length === 0 || participants.length === 0} onClick={() => manageWinner()} >Girar Roleta</button>
         </div>
       </div>
 
