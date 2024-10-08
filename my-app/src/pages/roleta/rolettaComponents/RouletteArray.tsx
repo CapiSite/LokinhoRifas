40a3f1d244a5587a6b13cc6a836356b0cf5c612f
hardkill <hardkill194@gmail.com>
2024-10-08 18:38:39 -0300

    fix/rouletteArray

--- my-app/src/pages/roleta/rolettaComponents/RouletteArray.tsx
+++ my-app/src/pages/roleta/rolettaComponents/RouletteArray.tsx
@@ -4,9 +4,9 @@ import RouletteItem from "./RouletteItem";
 import { RouletteContext } from "utils/interfaces";
 import { useRouletteContext } from "contexts/RouletteContext";
 import { v4 as uuidv4 } from 'uuid';
+import React from "react";
 
-
-const RouletteArray = () => {
+const RouletteArray = React.memo(() => {  // Aplicando React.memo no componente
   const {
     raffle = {
       createdAt: '',
@@ -30,24 +30,22 @@ const RouletteArray = () => {
   
   useEffect(() => {
     const debounce = setTimeout(() => {
-      if(rouletteLoadingState == false) return
-      const winner = document.getElementById('winner')
-
+      if (rouletteLoadingState === false) return;
+      const winner = document.getElementById('winner');
       
-      if(!winner) return
+      if (!winner) return;
   
-      getWinner(winner)
-
-      setIsButtonActive(true)
+      getWinner(winner);
+      setIsButtonActive(true);
     }, 400);
 
-    return () => clearTimeout(debounce)
-  }, [winners.length, rouletteLoadingState, winnerProperties?.id]);
+    return () => clearTimeout(debounce);
+  }, [winners.length, rouletteLoadingState, winnerProperties?.id, setIsButtonActive, getWinner]);
 
   return (
     <div className={style.RouletteArray} id="Roulette">
-      {(fillerParticipants && rouletteLoadingState) && fillerParticipants.map((item) => (
-        <RouletteItem key={uuidv4()} props={{
+      {(fillerParticipants && rouletteLoadingState) && fillerParticipants.map((item, index) => (
+        <RouletteItem key={`${item.id}-${index}`} props={{
           ...item,
           nickName: item.user.name + '#' + item.number,
           profilePicture: item.user.picture || '',
@@ -56,31 +54,38 @@ const RouletteArray = () => {
           number: item.number
         }} />
       ))}
-      {(winners && rouletteLoadingState) &&
-        winners.map((item, index) => (
-          <RouletteItem key={uuidv4()} props={{
-            ...item,
-            nickName: item.user.name + '#' + item.number,
-            profilePicture: item.user.picture,
-            personName: item.user.name,
-            isWinner: item.isWinner ? item.isWinner : false,
-            number: item.number,
-            distanceFromCenter: item.distanceFromCenter,
-            debugWinners: true,
-          }} />
-        ))}
-      {(fillerParticipants && rouletteLoadingState) && fillerParticipants.map((item) => (
-          <RouletteItem key={uuidv4()} props={{
-            ...item,
-            nickName: item.user.name + '#' + item.number,
-            profilePicture: item.user.picture,
-            personName: item.user.name,
-            isWinner: false,
-            number: item.number
-          }} />
-        ))}
+      {(winners && rouletteLoadingState) && winners.map((item, index) => (
+        <RouletteItem key={`${item.id}-${index}`} props={{
+          ...item,
+          nickName: item.user.name + '#' + item.number,
+          profilePicture: item.user.picture,
+          personName: item.user.name,
+          isWinner: item.isWinner ? item.isWinner : false,
+          number: item.number,
+          distanceFromCenter: item.distanceFromCenter,
+          debugWinners: true,
+        }} />
+      ))}
+      {(fillerParticipants && rouletteLoadingState) && fillerParticipants.map((item, index) => (
+        <RouletteItem key={`${item.id}-${index}`} props={{
+          ...item,
+          nickName: item.user.name + '#' + item.number,
+          profilePicture: item.user.picture,
+          personName: item.user.name,
+          isWinner: false,
+          number: item.number
+        }} />
+      ))}
     </div>
   );
-};
+}, (prevProps:any, nextProps:any) => {
+  // Custom comparator para evitar re-renderizações desnecessárias
+  return (
+    prevProps.winners === nextProps.winners &&
+    prevProps.fillerParticipants === nextProps.fillerParticipants &&
+    prevProps.rouletteLoadingState === nextProps.rouletteLoadingState &&
+    prevProps.winnerProperties?.id === nextProps.winnerProperties?.id
+  );
+});
 
 export default RouletteArray;
--- my-app/src/pages/roleta/rolettaComponents/RouletteItem.tsx
+++ my-app/src/pages/roleta/rolettaComponents/RouletteItem.tsx
@@ -2,55 +2,69 @@ import style from "../roletta.module.css";
 import Image from "next/image";
 import { CardItemType } from "utils/interfaces";
 import cn from "classnames";
-
 import defaultPicture from "../../../assets/defaultProfilePic.svg";
 import { useEffect, useState } from "react";
 import React from "react";
 
-const RouletteItem = React.memo(({ props }: { props?: CardItemType }) => {
-  // Ensure props is defined and has the necessary properties
-  const {
-    profilePicture = '',
-    personName = 'Unknown',
-    number = 0,
-    debugWinners = false,
-    isWinner = false,
-  } = props || {}; // Use empty object if props is undefined
+const RouletteItem = React.memo(
+  ({ props }: { props?: CardItemType }) => {
+    // Desestruturando props
+    const {
+      profilePicture = '',
+      personName = 'Unknown',
+      number = 0,
+      isWinner = false,
+    } = props || {};
 
-  const [imgSrc, setImgSrc] = useState<string>(defaultPicture);
+    const [imgSrc, setImgSrc] = useState<string>(defaultPicture);
 
-  useEffect(() => {
-    if (!profilePicture) return;
+    // Atualize a imagem apenas se a 'profilePicture' mudar
+    useEffect(() => {
+      console.log("oi")
+      if (profilePicture && profilePicture !== imgSrc) {
+        if (profilePicture.includes('https://static-cdn.jtvnw.net')) {
+          setImgSrc(profilePicture);
+        } else if (!profilePicture.includes('default')) {
+          setImgSrc(profilePicture);
+        }
+      }
+    }, [profilePicture]); // Somente 'profilePicture' é a dependência
 
-    if (profilePicture.includes('https://static-cdn.jtvnw.net')) {
-      setImgSrc(profilePicture);
-    } else if (!profilePicture.includes('default') && profilePicture !== imgSrc) {
-      setImgSrc(profilePicture);
-    }
-  }, [profilePicture, imgSrc]);
-
-  return (
-    <div
-      data-number={number}
-      id={isWinner ? 'winner' : ''}
-      className={cn(style.PersonCard)}
-    >
-      <div className={style.PersonCardWrapper}>
-        <div className={style.ProfilePicture}>
-          <Image
-            src={imgSrc}
-            width={500}
-            height={500}
-            alt={`Foto de perfil de ${personName}`}
-            onError={() => setImgSrc(defaultPicture)}
-          />
-        </div>
-        <div className={style.ProfileInfo}>
-          <h3>{personName}#{number}</h3>
+    return (
+      <div
+        data-number={number}
+        id={isWinner ? 'winner' : ''}
+        className={cn(style.PersonCard)}
+      >
+        <div className={style.PersonCardWrapper}>
+          <div className={style.ProfilePicture}>
+            <Image
+              src={imgSrc}
+              width={500}
+              height={500}
+              alt={`Foto de perfil de ${personName}`}
+              onError={() => {
+                if (imgSrc !== defaultPicture) {
+                  setImgSrc(defaultPicture);
+                }
+              }}
+            />
+          </div>
+          <div className={style.ProfileInfo}>
+            <h3>{personName}#{number}</h3>
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+  // Função de comparação para evitar re-renderizações desnecessárias
+  (prevProps:any, nextProps:any) => {
+    return (
+      prevProps.props.profilePicture === nextProps.props.profilePicture &&
+      prevProps.props.isWinner === nextProps.props.isWinner &&
+      prevProps.props.number === nextProps.props.number
+    );
+  }
+);
 
 export default RouletteItem;
